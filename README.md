ce88355633e88e64ae3e68e0cd06351b224261bf
anders11 <anderslatif@gmail.com>
2016-10-21 12:01:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# This is a hobby project to learn React and play around with code
+
+The idea is a single page app that can help you generate and retrieve lists for movies, music and litterature.
+
+It uses the following APIs. 
---
+++ src/js/components/Layout.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+
+            </div>
+        )
+    }
+
+
+
+}
