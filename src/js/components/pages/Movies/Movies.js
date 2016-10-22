5160f42dc21dce91ee3e4b26d20ee4042b2a470f
anders11 <anderslatif@gmail.com>
2016-10-22 22:43:17 +0200

    finally set up the finaly structure

--- src/js/components/pages/Movies/Movies.js
+++ src/js/components/pages/Movies/Movies.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import SubNav from "./../Nav/SubNav/Sub_Nav";
+
 
 export default class Movies extends React.Component {
     constructor() {
@@ -13,7 +13,6 @@ export default class Movies extends React.Component {
     render() {
         return (
             <div>
-                <SubNav />
                 <div>
                     <span>Hello hello.. I am in Movies.js</span>
                 </div>
